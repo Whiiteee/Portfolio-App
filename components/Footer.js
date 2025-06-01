2ba574394c1a18f31b652bcc04b647bf6137122e
Whiiteee <soumyamishra2099@gmail.com>
2025-06-02 01:29:11 +0530

    Update Footer component with personal information

--- components/Footer.js
+++ components/Footer.js
@@ -78,7 +78,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 <a href="tel:+1234567890" className="text-gray-400 hover:text-white transition-colors">
-                  +1 (234) 567-890
+                  +91 70589 20175
                 </a>
               </li>
               <li className="flex items-center">
@@ -87,7 +87,7 @@ export default function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
                 <span className="text-gray-400">
-                  San Francisco, CA, USA
+                  Nagpur, Mahrashtra, India
                 </span>
               </li>
             </ul>
